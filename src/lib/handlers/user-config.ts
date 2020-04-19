c18a75f3fb37f84ec68a2a8b8439e9694880e517
Alexander Schumskich <alexander.schumskich@googlemail.com>
2020-04-20 00:22:34 +0200

    Added a separate method to create an initial user config.

--- src/lib/handlers/user-config.ts
+++ src/lib/handlers/user-config.ts
@@ -1,5 +1,7 @@
 import {UserConfig} from "@/lib/model/user-config";
 import {Fs} from "@/lib/helpers/fs";
+import * as path from "path";
+import {DirectoryNotEmptyError} from "@/lib/model/errors";
 
 export const loadConfigFromPath = async (path: string): Promise<UserConfig> => {
     const fileContent = await Fs.readFile(path, {encoding: 'utf-8'});
@@ -8,4 +10,15 @@ export const loadConfigFromPath = async (path: string): Promise<UserConfig> => {
 
 export const writeConfigToPath = async (path: string, config: UserConfig): Promise<unknown> => {
     return Fs.writeFile(path, JSON.stringify(config));
+};
+
+export const writeNewConfigToPath = async (filePath: string, config: UserConfig): Promise<unknown> => {
+    const directoryPath = path.dirname(filePath);
+    const filesInDirectory = await Fs.readdir(directoryPath);
+
+    if (filesInDirectory && filesInDirectory.length > 0) {
+        throw new DirectoryNotEmptyError("The directory of a new project must be empty.", directoryPath)
+    }
+
+    return writeConfigToPath(filePath, config);
 };
