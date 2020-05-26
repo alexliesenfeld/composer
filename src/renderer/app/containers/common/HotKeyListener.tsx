6d4688305cd5911f57f9f6f2cb4769d4f82ad6a0
Alexander Schumskich <alexander.schumskich@googlemail.com>
2020-05-26 21:38:41 +0200

    Hotkey bugfix

--- src/renderer/app/containers/common/HotKeyListener.tsx
+++ src/renderer/app/containers/common/HotKeyListener.tsx
@@ -4,6 +4,17 @@ import { useCallback } from 'react';
 import * as React from 'react';
 import { GlobalHotKeys, KeyMap } from 'react-hotkeys';
 
+import { configure } from 'react-hotkeys';
+
+configure({
+    /**
+     * The HTML tags that React HotKeys should ignore key events from. This only works
+     * if you are using the default ignoreEventsCondition function.
+     * @type {String[]}
+     */
+    ignoreTags: [], // default: 'input', 'select', 'textarea'
+});
+
 const keyMap: KeyMap = {
     SAVE_WORKSPACE: ['command+s', 'ctrl+s'],
 };
